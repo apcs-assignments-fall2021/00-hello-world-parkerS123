83300af53b10c1ed65389b8572bc2c1822bd66fc
parkerschwantes <23pschwantes@athenian.org>
2021-09-01 10:15:24 -0700

    successful

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -2,6 +2,7 @@ public class MyMain {
     public static void main(String[] args) {
         // Write some code here!
         System.out.println("Hello world!");
+        // test comment
 
     }
 }
